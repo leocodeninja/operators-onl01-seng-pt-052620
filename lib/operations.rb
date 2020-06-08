61621c72f136cbb1208d48ffea8cf4d1a99cee8b
Leonardo Newton <leo253@protonmail.com>
2020-06-08 15:42:24 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+  return true if speed > 60
+  return true if speed < 40
+  return false if speed > 40 || speed < 60
 end
 
 
 
 def not_safe?(speed)
-	
+  return true if speed > 60 || speed < 40
+  return false if speed > 40 || speed < 60
+
+  speed > 60 || speed < 40 ?   true :  false
+
 end
-	
-
-
